bd151ec1ab6480ade98d4fc8a8c05a6632e516ba
Ginters Juris Vehi <vehiginters@gmail.com>
2018-06-16 11:24:56 +0300

    Made design for Pokemon gallery

--- resources/views/custom.blade.php
+++ resources/views/custom.blade.php
@@ -6,11 +6,47 @@
 </div>
 <div class="container">
   <div class="row">
-    <div class="col-lg-6 boxy">
-      <img src="/img/Drifblim.png"></img>
+    <div class="col-lg-3 pt-3 pl-5 pr-3 pb-3 ml-5 text-center">
+      <img src="/img/Drifblim.png" class="imgs boxy"></img>
+      <div class="text-center ml-5">
+      <p class="lead"><b>Drifblim</b></p>
+      <p>Votes:</p>
+      <p id="Drifblim_votes">0</p>
+      <button class="btn btn-sm btn-primary btn-block" type="submit">Vote!</button>
     </div>
-    <div class="col-lg-6">
-
+    </div>
+    <div class="col-lg-2">
+    </div>
+    <div class="col-lg-3 pt-3 pl-5 pr-3 pb-3 ml-5 text-center">
+      <img src="/img/Garchomp.png" class="imgs boxy"></img>
+      <div class="text-center ml-5">
+      <p class="lead"><b>Garchomp</b></p>
+      <p>Votes:</p>
+      <p id="Garchomp_votes">0</p>
+      <button class="btn btn-sm btn-primary btn-block" type="submit">Vote!</button>
+    </div>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-lg-3 pt-3 pl-5 pr-3 pb-3 ml-5 text-center">
+      <img src="/img/Chandelure.png" class="imgs boxy"></img>
+      <div class="text-center ml-5">
+      <p class="lead"><b>Chandelure</b></p>
+      <p>Votes:</p>
+      <p id="Chandelure_votes">0</p>
+      <button class="btn btn-sm btn-primary btn-block" type="submit">Vote!</button>
+    </div>
+    </div>
+    <div class="col-lg-2">
+    </div>
+    <div class="col-lg-3 pt-3 pl-5 pr-3 pb-3 ml-5 text-center">
+      <img src="/img/Skarmory.png" class="imgs boxy"></img>
+      <div class="text-center ml-5">
+      <p class="lead"><b>Skarmory</b></p>
+      <p>Votes:</p>
+      <p id="Skarmory_votes">0</p>
+      <button class="btn btn-sm btn-primary btn-block" type="submit">Vote!</button>
+    </div>
     </div>
   </div>
 </div>
